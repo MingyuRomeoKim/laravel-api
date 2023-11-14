ee110e68ae0f2e586a23f12600be642a5e703108
MingyuRomeoKim <rlaalsrb0466@naver.com>
2023-11-14 15:41:02 +0900

    블로그 게시글 상세 페이지(콘텐츠) 관련 기능 추가

--- app/Http/Controllers/Api/TistoryController.php
+++ app/Http/Controllers/Api/TistoryController.php
@@ -9,12 +9,6 @@ use Illuminate\Support\Arr;
 
 class TistoryController extends Controller
 {
-    private string $outputDataPath;
-
-    public function __construct()
-    {
-        $this->outputDataPath = config('tistory.output_data_path') ?? "";
-    }
 
     public function index(Request $request)
     {
@@ -22,20 +16,21 @@ class TistoryController extends Controller
         $access_token = $request->input('token');
         // 블로그 정보 관련
         $blogInfo = $tistoryLibrary->getBlogInfo(access_token: $access_token);
-        $detailBlogInfo = $this->makeDetailBlogInfo(blogInfo: $blogInfo, selectBlogIndex: 0);
+        $detailBlogInfo = $tistoryLibrary->makeDetailBlogInfo(blogInfo: $blogInfo, selectBlogIndex: 0);
         $blogName = $detailBlogInfo['blog']['name'];
 
         // 블로그 카테고리 관련
         $category = $tistoryLibrary->getCategoryList(access_token: $access_token, blog_name: $blogName);
-        $detailCategory = $this->makeDetailCategory($category);
+        $detailCategory = $tistoryLibrary->makeDetailCategory($category);
 
         // 블로그 게시글 관련
         $startPage = 1;
         $postList = $tistoryLibrary->getPostList(access_token: $access_token, blog_name: $blogName, page: $startPage);
-        $allPostList = $this->makeAllPostList(data: $postList, access_token: $access_token, blogName: $blogName, page: $startPage++, tistoryLibrary: $tistoryLibrary);
+        $allPostList = $tistoryLibrary->makeAllPostList(data: $postList, access_token: $access_token, blogName: $blogName, page: $startPage++);
 
-        $postContent = $tistoryLibrary->getPostContent(access_token: $access_token, blog_name: $blogName, post_id: '280');
-        dd('블로그정보', $detailBlogInfo, '카테고리', $detailCategory, '글 목록', $allPostList, '글 읽기', $postContent);
+        // 블로그 게시글 상세뷰 관련
+        $postDetailViewPath = $tistoryLibrary->makeAllPostDetailView(access_token: $access_token, blogName: $blogName, data: $allPostList);
+        dd('블로그정보', $detailBlogInfo, '카테고리', $detailCategory, '글 목록', $allPostList, '글 읽기', $postDetailViewPath);
     }
 
     public function accessToken(Request $request)
@@ -52,126 +47,5 @@ class TistoryController extends Controller
         return redirect()->route('tistory.index', ['token' => $token]);
     }
 
-    private function makeAllPostList(array $data, string $access_token, string $blogName, int $page, TistoryLibrary $tistoryLibrary): array
-    {
-        try {
-            $returnData = [];
-            $item = $data['tistory']['item'];
-            $totalPage = round($item['totalCount'] / $item['count']);
-
-            if ($totalPage > 1) {
-                $returnData['posts'][] = $item['posts'];
-                for ($i = $page; $i <= $totalPage; $i++) {
-                    $newData = $tistoryLibrary->getPostList(access_token: $access_token, blog_name: $blogName, page: $i);
-                    $returnData['posts'][] = $newData['tistory']['item']['posts'] ?? null;
-                }
-
-            }
-
-            // 카테고리ID를 그룹으로 맵핑지어 posts 배열 데이터 재정의
-            $posts = $returnData['posts'];
-            $groupedPosts = [];
-
-            foreach ($posts as $postGroup) {
-                foreach ($postGroup as $post) {
-                    $categoryId = $post['categoryId'];
-
-                    // 해당 categoryId가 이미 배열에 존재하면, 현재 post를 그 배열에 추가
-                    if (!isset($groupedPosts[$categoryId])) {
-                        $groupedPosts[$categoryId] = [];
-                    }
-
-                    $groupedPosts[$categoryId][] = $post;
-                }
-            }
-
-            foreach ($groupedPosts as $categoryId => $post) {
-                $this->makeJsonFile(data: $post, filePath: $this->outputDataPath . "categories/", fileName: $categoryId . ".json");
-            }
-
-            $returnData['posts'] = $groupedPosts;
-        } catch (\Exception $exception) {
-            die("makeAllPostList ERROR :: " . $exception->getMessage());
-        }
-
-        return $returnData;
-    }
-
-    private function makeJsonFile(array $data, ?string $filePath, string $fileName): void
-    {
-        try {
-            $fileFullPath = !is_null($filePath) ? $filePath . $fileName : $fileName;
-
-            // 디렉토리가 존재하는지 확인하고, 없으면 생성
-            if (!is_null($filePath) && !file_exists($filePath)) {
-                mkdir($filePath, 0777, true); // true는 중첩된 디렉토리 생성을 허용합니다.
-            }
-
-            $jsonData = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-            file_put_contents($fileFullPath, $jsonData);
-        } catch (\Exception $exception) {
-            die("makeJsonFile ERROR :: " . $exception->getMessage());
-        }
-    }
-
-    private function makeDetailCategory(array $category): array
-    {
-
-        try {
-            $item = $category['tistory']['item'];
-            $parents = array();
-            $parentsKey = array();
-
-            foreach ($item['categories'] as $key => $category) {
-                if (!$category['parent']) {
-                    $parents[] = $category;
-                    $parentsKey[] = $key;
-                }
-            }
-
-            foreach ($parentsKey as $key) {
-                unset($item['categories'][$key]);
-            }
-
-
-            foreach ($parents as $parentKey => $parent) {
-                $subCategoryKey = array();
-                foreach ($item['categories'] as $key => $category) {
-                    if ($parent['id'] === $category['parent']) {
-                        $parents[$parentKey]['subCategories'][] = $category;
-                        $subCategoryKey[] = $key;
-                    }
-                }
-
-                foreach ($subCategoryKey as $key) {
-                    unset($item['categories'][$key]);
-                }
-            }
-
-            $item['categories'] = $parents;
-
-            $this->makeJsonFile(data: $item, filePath: $this->outputDataPath, fileName: "categories.json");
-
-        } catch (\Exception $exception) {
-            die("makeDetailCategory ERROR :: " . $exception->getMessage());
-        }
-
-        return $item;
-    }
-
-    private function makeDetailBlogInfo(array $blogInfo, int $selectBlogIndex): array
-    {
-        $returnData = array();
-        try {
-            $item = $blogInfo['tistory']['item'];
-            $returnData['userId'] = $item['userId'];
-            $returnData['blog'] = $item['blogs'][$selectBlogIndex];
-            $this->makeJsonFile(data: $returnData, filePath: $this->outputDataPath, fileName: "blogInfo.json");
-        } catch (\Exception $exception) {
-            die("makeDetailBlogInfo ERROR :: " . $exception->getMessage());
-        }
-
-        return $returnData;
-    }
 
 }
--- app/Library/TistoryLibrary.php
+++ app/Library/TistoryLibrary.php
@@ -12,6 +12,7 @@ class TistoryLibrary
     private string $redirect_url;
     private string $grant_type;
     private string $output_type;
+    private string $outputDataPath;
 
     public function __construct()
     {
@@ -20,8 +21,16 @@ class TistoryLibrary
         $this->redirect_url = config('tistory.redirect_url');
         $this->grant_type = config('tistory.grant_type');
         $this->output_type = config('tistory.output_type');
+        $this->outputDataPath = config('tistory.output_data_path') ?? "";
     }
 
+    /**
+     * @param string $access_token
+     * @param string $blog_name
+     * @param string|int $post_id
+     * @return array|null
+     * @description https://www.tistory.com/apis/post/read 호출 및 응답 데이터 관련 함수
+     */
     public function getPostContent(string $access_token, string $blog_name, string|int $post_id): ?array
     {
         $requestData = [
@@ -37,6 +46,8 @@ class TistoryLibrary
             die("getPostContent :: result not found");
         }
 
+        // XML에서 유효하지 않은 문자 제거
+        $result = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]/', '', $result);
         // XML 문자열을 SimpleXMLElement 객체로 변환
         $xmlObject = simplexml_load_string($result);
         // SimpleXMLElement 객체를 JSON 문자열로 변환
@@ -51,6 +62,12 @@ class TistoryLibrary
         return $result;
     }
 
+    /**
+     * @param string $access_token
+     * @param string $blog_name
+     * @return array
+     * @description https://www.tistory.com/apis/category/list 호출 및 응답 데이터 관련 함수
+     */
     public function getCategoryList(string $access_token, string $blog_name): array
     {
         $requestData = [
@@ -75,6 +92,13 @@ class TistoryLibrary
         return $result;
     }
 
+    /**
+     * @param string $access_token
+     * @param string $blog_name
+     * @param string|int $page
+     * @return array|null
+     * @description https://www.tistory.com/apis/post/list 호출 및 응답 데이터 관련 함수
+     */
     public function getPostList(string $access_token, string $blog_name, string|int $page): ?array
     {
         $requestData = [
@@ -99,6 +123,11 @@ class TistoryLibrary
         return $result;
     }
 
+    /**
+     * @param string|null $code
+     * @return string
+     * @description https://www.tistory.com/oauth/access_token 호출 및 응답 데이터 관련 함수
+     */
     public function getAccessToken(?string $code): string
     {
 
@@ -140,6 +169,190 @@ class TistoryLibrary
         }
 
         return $result;
+    }
+
+    /**
+     * @param array $blogInfo
+     * @param int $selectBlogIndex
+     * @return array
+     * @description 블로그 정보를 지정한 path에 blogInfo.json명으로 저장한 후, 뒤에 다른 api를 사용하기 위해 필요 정보를 array로 리턴한다.
+     */
+    public function makeDetailBlogInfo(array $blogInfo, int $selectBlogIndex): array
+    {
+        $returnData = array();
+        try {
+            $item = $blogInfo['tistory']['item'];
+            $returnData['userId'] = $item['userId'];
+            $returnData['blog'] = $item['blogs'][$selectBlogIndex];
+            $this->makeJsonFile(data: $returnData, filePath: $this->outputDataPath, fileName: "blogInfo.json");
+        } catch (\Exception $exception) {
+            die("makeDetailBlogInfo ERROR :: " . $exception->getMessage());
+        }
+
+        return $returnData;
+    }
+
+    /**
+     * @param array $category
+     * @return array
+     * @description category parent값을 기준으로 subcategory를 parent 하위 형태의 배열로 재가공하여 지정한 path의 categories.json 명으로 저장한 후 저장된 full path를 반환한다.
+     */
+    public function makeDetailCategory(array $category): array
+    {
+
+        try {
+            $returnData = array();
+            $item = $category['tistory']['item'];
+            $parents = array();
+            $parentsKey = array();
+
+            foreach ($item['categories'] as $key => $category) {
+                if (!$category['parent']) {
+                    $parents[] = $category;
+                    $parentsKey[] = $key;
+                }
+            }
+
+            foreach ($parentsKey as $key) {
+                unset($item['categories'][$key]);
+            }
+
+
+            foreach ($parents as $parentKey => $parent) {
+                $subCategoryKey = array();
+                foreach ($item['categories'] as $key => $category) {
+                    if ($parent['id'] === $category['parent']) {
+                        $parents[$parentKey]['subCategories'][] = $category;
+                        $subCategoryKey[] = $key;
+                    }
+                }
+
+                foreach ($subCategoryKey as $key) {
+                    unset($item['categories'][$key]);
+                }
+            }
+
+            $item['categories'] = $parents;
+
+            $returnData['outFileFullPath'][] = $this->makeJsonFile(data: $item, filePath: $this->outputDataPath, fileName: "categories.json");
+
+        } catch (\Exception $exception) {
+            die("makeDetailCategory ERROR :: " . $exception->getMessage());
+        }
+
+        return $returnData;
+    }
+
+    /**
+     * @param array $data
+     * @param string $access_token
+     * @param string $blogName
+     * @param int $page
+     * @return array
+     * @description 블로그 전체 게시글을 가져와 categoryId를 기준으로 데이터를 가공하여 지정한 path의 categories/{category-id}.json 명으로 저장한 후 필요한 데이터를 array로 리턴한다.
+     */
+    public function makeAllPostList(array $data, string $access_token, string $blogName, int $page): array
+    {
+        try {
+            $returnData = array();
+
+            $newItems = array();
+            $item = $data['tistory']['item'];
+            $totalPage = round($item['totalCount'] / $item['count']);
+
+            if ($totalPage > 1) {
+                $newItems['posts'][] = $item['posts'];
+                for ($i = $page; $i <= $totalPage; $i++) {
+                    $newData = $this->getPostList(access_token: $access_token, blog_name: $blogName, page: $i);
+                    $newItems['posts'][] = $newData['tistory']['item']['posts'] ?? null;
+                }
+
+            }
+
+            // 카테고리ID를 그룹으로 맵핑지어 posts 배열 데이터 재정의
+            $posts = $newItems['posts'];
+            $groupedPosts = array();
+
+            foreach ($posts as $postGroup) {
+                foreach ($postGroup as $post) {
+                    $categoryId = $post['categoryId'];
+
+                    // 해당 categoryId가 이미 배열에 존재하면, 현재 post를 그 배열에 추가
+                    if (!isset($groupedPosts[$categoryId])) {
+                        $groupedPosts[$categoryId] = [];
+                    }
+
+                    $groupedPosts[$categoryId][] = $post;
+                }
+            }
+
+            foreach ($groupedPosts as $categoryId => $post) {
+                $this->makeJsonFile(data: $post, filePath: $this->outputDataPath . "categories/", fileName: $categoryId . ".json");
+            }
+
+            $returnData['posts'] = $groupedPosts;
+
+        } catch (\Exception $exception) {
+            die("makeAllPostList ERROR :: " . $exception->getMessage());
+        }
+
+        return $returnData;
+    }
+
+    /**
+     * @param string $access_token
+     * @param string $blogName
+     * @param array $data
+     * @return array
+     * @description 블로그 게시글 content 내용들을 전체 지정한 path의 posts/{category-id}/{post-id}.json 명으로 저장한 후 저장된 full path를 반환한다.
+     */
+    public function makeAllPostDetailView(string $access_token, string $blogName, array $data): array
+    {
+        $returnData = array();
+
+        try {
+            dump($data);
+            $postLists = $data['posts'];
+            foreach ($postLists as $categoryId => $postList) {
+                foreach ($postList as $key => $postItem) {
+                    $newItem = $this->getPostContent(access_token: $access_token, blog_name: $blogName, post_id: $postItem['id']);
+                    $returnData['outFileFullPath'][] = $this->makeJsonFile(data: $newItem['item'], filePath: $this->outputDataPath . "posts/" . $categoryId . "/", fileName: $newItem['item']['id'] . ".json");
+                }
+                usleep(500000); // 0.5초 딜레이
+            }
+        } catch (\Exception $exception) {
+            die("makeAllPostDetailView ERROR :: " . $exception->getMessage());
+        }
+
+        return $returnData;
+    }
+
+    /**
+     * @param array $data
+     * @param string|null $filePath
+     * @param string $fileName
+     * @return void
+     * @description 지정한 filePath 및 fileName 장소에 data를 json형태로 저장한 후 파일 저장 경로를 반환한다.
+     */
+    private function makeJsonFile(array $data, ?string $filePath, string $fileName): string
+    {
+        try {
+            $fileFullPath = !is_null($filePath) ? $filePath . $fileName : $fileName;
+
+            // 디렉토리가 존재하는지 확인하고, 없으면 생성
+            if (!is_null($filePath) && !file_exists($filePath)) {
+                mkdir($filePath, 0777, true); // true는 중첩된 디렉토리 생성을 허용
+            }
+
+            $jsonData = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            file_put_contents($fileFullPath, $jsonData);
+
+            // 파일의 전체 경로 반환
+            return $fileFullPath;
+
+        } catch (\Exception $exception) {
+            die("makeJsonFile ERROR :: " . $exception->getMessage());
+        }
     }
 
     private function initApiLibrary(string $method, string $url, mixed $data): void
